d8cd1a4fd460a8a338ac2f116a3a810fc7714854
Miguel Esmoris <miguelesmoris@gmail.com>
2022-02-07 12:36:56 +0100

    pushTests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
-import { TRIPS } from "./data/astro_bookings";
-import { Trip } from "./models/trip";
+import { OPERATORS, TRIPS } from "./data/astro_bookings";
+import { Operator, OperatorStatus } from "./models/operator";
+import { Trip, TripKinds } from "./models/trip";
 
 @Component({
   selector: "app-root",
@@ -10,7 +11,19 @@ import { Trip } from "./models/trip";
 export class AppComponent {
   title = "aldaba-angular-intro";
   public trips: Trip[] = TRIPS;
+  public operators: Operator[] = OPERATORS;
   constructor() {
     console.log(this.trips);
   }
+
+  public getOperatorClass( operator: Operator) : string{
+    const active = OperatorStatus [OperatorStatus.ACTIVE];
+    return operator.status.toString() == active ? "active":"pending" ;
+  }
+
+  public getFlightsClass( trip: Trip) : string{
+    const tripOnly = TripKinds [TripKinds.TRIP_ONLY];
+    return trip.kind.toString() == tripOnly ? "idaVuelta":"conEstancia" ;
+  }
+
 }
