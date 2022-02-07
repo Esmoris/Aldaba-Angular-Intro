5e68030c31a3c582462fb9c5489ea5c770355f2c
Alberto Basalo <albertobasalo@hotmail.com>
2022-02-07 13:17:04 +0100

    feat: session 1

--- TO_DO.md
+++ TO_DO.md
@@ -7,7 +7,7 @@
 - [x] Mostrar una encabezado con el contador de viajes
 - [x] Una li para cada operador mostrando nombre, rango y estado
 - [x] Distinguir con colores los que estén activos
-- [ ] Distinguir con iconos el rango de viaje
-- [ ] Una div para cada viaje mostrando nombre, fechas, plazas disponibles y el importe
-- [ ] Distinguir con colores los que sean de ida y vuelta de los que incluyen estancia
-- [ ] Si hay plazas disponibles, mostrar un botón para reservar
+- [x] Distinguir con iconos el rango de viaje
+- [x] Una div para cada viaje mostrando nombre, fechas, plazas disponibles y el importe
+- [x] Distinguir con colores los que sean de ida y vuelta de los que incluyen estancia
+- [x] Si hay plazas disponibles, mostrar un botón para reservar
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { OPERATORS, TRIPS } from "./data/astro_bookings";
-import { Operator, OperatorStatus } from "./models/operator";
-import { Trip } from "./models/trip";
+import { Operator, OperatorRanges, OperatorStatus } from "./models/operator";
+import { Trip, TripKinds } from "./models/trip";
 
 @Component({
   selector: "app-root",
@@ -18,5 +18,16 @@ export class AppComponent {
   public getOperatorClass(operator: Operator): string {
     const active = OperatorStatus[OperatorStatus.ACTIVE];
     return operator.status.toString() == active ? "active" : "pending";
+  }
+  public getOperatorIcon(operator: Operator): string {
+    const orbital = OperatorRanges[OperatorRanges.ORBITAL];
+    return operator.range.toString() == orbital ? "🌍" : "💫";
+  }
+  public getTripClass(trip: Trip): string {
+    const tripOnly = TripKinds[TripKinds.TRIP_ONLY];
+    return trip.kind.toString() == tripOnly ? "trip_only" : "with_stay";
+  }
+  public canReserveBooking(trip: Trip): boolean {
+    return trip.places > 0;
   }
 }
