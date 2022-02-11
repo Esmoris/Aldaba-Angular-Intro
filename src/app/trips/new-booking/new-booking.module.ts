e042b24a24cae0e9a66bc2df44b8fbc296d36344
Alberto Basalo <albertobasalo@hotmail.com>
2022-02-11 11:24:59 +0100

    feat: new booking route

---
+++ src/app/trips/new-booking/new-booking.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NewBookingRoutingModule } from './new-booking-routing.module';
+import { NewBookingComponent } from './new-booking.component';
+
+
+@NgModule({
+  declarations: [NewBookingComponent],
+  imports: [
+    CommonModule,
+    NewBookingRoutingModule
+  ]
+})
+export class NewBookingModule { }
