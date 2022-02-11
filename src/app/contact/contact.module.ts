631bdbc1f1b316ca010db14cf91c0312356f0ff6
Alberto Basalo <albertobasalo@hotmail.com>
2022-02-11 11:57:29 +0100

    feat: contact form

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { ContactComponent } from "./contact.component";
 
 @NgModule({
   declarations: [ContactComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [ContactComponent],
 })
 export class ContactModule {}
